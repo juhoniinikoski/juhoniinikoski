c5f4482478c11ca637438692757c485a1ab68633
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-06-06 08:23:16 +0300

    Feature: Fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://juhoniinikoski.com",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: aws(),
