720a01adcb4dcf0332b66532f9ed5109e2ab569f
Juho Niinikoski <niinikoskijuho@gmail.com>
2026-04-01 12:10:40 +0300

    Chore: Update footer

--- sst.config.ts
+++ sst.config.ts
@@ -16,11 +16,13 @@ export default $config({
   },
   async run() {
     new sst.aws.Astro("AstroSite", {
-      domain: {
-        name: "juhoniinikoski.com",
-        dns: false,
-        cert: "arn:aws:acm:us-east-1:891377093701:certificate/5f242e2d-d71f-4fe8-9e75-b365a4fe7c22",
-      },
+      domain: $dev
+        ? undefined
+        : {
+            name: "juhoniinikoski.com",
+            dns: false,
+            cert: "arn:aws:acm:us-east-1:891377093701:certificate/5f242e2d-d71f-4fe8-9e75-b365a4fe7c22",
+          },
     });
   },
 });
